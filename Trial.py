d8e99b25aa24dda9dff0350ffea32b26bc0ad142
EvgeniiKostromin <evgenii.kostromin@polytechnique.edu>
2023-12-08 03:08:04 +0100

    10th trial

--- Trial.py
+++ Trial.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[92]:
+# In[1016]:
 
 
 from pathlib import Path
@@ -13,13 +13,13 @@ import matplotlib.pyplot as plt
 import sklearn
 
 
-# In[93]:
+# In[1017]:
 
 
 df_train = pd.read_parquet(Path("data") / "train.parquet")
 
 
-# In[94]:
+# In[1018]:
 
 
 def _encode_dates(X):
@@ -30,12 +30,13 @@ def _encode_dates(X):
     X["day"] = X["date"].dt.day
     X["weekday"] = X["date"].dt.weekday
     X["hour"] = X["date"].dt.hour
+    X['week'] = X["date"].dt.isocalendar().week
     
     # Finally we can drop the original columns from the dataframe
     return X.drop(columns=["date"])
 
 
-# In[95]:
+# In[1019]:
 
 
 def _merge_external_data(X):
@@ -49,7 +50,7 @@ def _merge_external_data(X):
     # When using merge_asof left frame need to be sorted
     X["orig_index"] = np.arange(X.shape[0])
     X = pd.merge_asof(
-        X.sort_values("date"), df_ext[["date", "t", 'pres', 'raf10', 'u', 'vv', 'tend24', 'cod_tend', 'rr3', 'rr12']].sort_values("date"), on="date"
+        X.sort_values("date"), df_ext[["date", "t", 'pmer', 'rafper', 'u', 'hnuage2', 'tn12', 'cod_tend', 'rr24', 'rr12']].sort_values("date"), on="date"
     )
     # Sort back to the original order
     X = X.sort_values("orig_index")
@@ -57,34 +58,59 @@ def _merge_external_data(X):
     return X
 
 
-# In[96]:
+# In[1020]:
 
 
 df_train = _merge_external_data(df_train)
 
 
-# In[97]:
-
-
-df_train['counter_age_days'] = df_train['date'] - df_train['counter_installation_date']
-df_train['counter_age_days'] = df_train['counter_age_days'].dt.days
-
-
-# In[98]:
+# In[1021]:
 
 
 X_dates_encoding = _encode_dates(df_train[["date"]])
-df_train = pd.concat([df_train, X_dates_encoding], axis=1) 
+df_train = pd.concat([df_train, X_dates_encoding], axis=1)
 
 
-# In[99]:
+# In[1022]:
 
 
-y_train = df_train['log_bike_count']
-X_train = df_train.drop('log_bike_count', axis=1)
+from sklearn.preprocessing import OrdinalEncoder
+encoder = OrdinalEncoder()
+encoder.fit(df_train[["counter_name"]])
+df_train["counter_name"] = encoder.transform(df_train[["counter_name"]])
 
 
-# In[100]:
+# In[1023]:
+
+
+y = df_train['log_bike_count']
+X = df_train.drop('log_bike_count', axis=1)
+
+
+# from sklearn.preprocessing import OneHotEncoder
+# 
+# encoder = OneHotEncoder(sparse=False)
+# data_encoded = encoder.fit_transform(X['counter_name'])
+# X=
+
+# In[1024]:
+
+
+X_train, X_valid, y_train, y_valid = train_test_split(
+    X, y, random_state=2408
+)
+
+
+# In[1025]:
+
+
+selected_features = ['latitude', 't', 'u', 'month', 'weekday', 'hour', 'day', "counter_name"]
+X_train = X_train[selected_features]
+
+X_valid = X_valid[selected_features]
+
+
+# In[1026]:
 
 
 from sklearn.compose import ColumnTransformer
@@ -97,65 +123,61 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.impute import SimpleImputer
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import pandas as pd
+from xgboost import XGBRegressor
 
-date_cols = []
-scaling_columns = []
-categorical_encoder = OneHotEncoder(handle_unknown="ignore")
-categorical_cols = ["counter_name"]
-passthrough_cols = ['year', 'month', 'weekday', 'hour', 'latitude', 't', 'u', 'cod_tend', 'rr3']
+# Assuming df is your DataFrame containing the required columns
+# Replace 'df' with the actual variable containing your dataset
 
-imputer = SimpleImputer(strategy='mean')
 
-preprocessor = ColumnTransformer(
-    [
-        ("cat", categorical_encoder, categorical_cols),
-        ('passthrough', 'passthrough', passthrough_cols)
-    ]
-)
-
-regressor = GradientBoostingRegressor(n_estimators=200, learning_rate=0.1,  max_depth=4)
-
-pipe = make_pipeline(preprocessor, imputer, regressor)
+# Initialize and train a Gradient Boosting Regressor
+# learning_rate=0.01, max_depth=7, n_estimators=300
+pipe =  XGBRegressor(learning_rate=0.22, max_depth=6, n_estimators=600, gamma=0.01, min_child_weight=7, reg_alpha=0.15)
 pipe.fit(X_train, y_train)
 
 
-# In[101]:
+# In[1027]:
+
+
+print(
+    f"Train set, RMSE={mean_squared_error(y_train, pipe.predict(X_train), squared=False):.2f}"
+)
+print(
+    f"Train set, RMSE={mean_squared_error(y_valid, pipe.predict(X_valid), squared=False):.2f}"
+)
+
+
+# In[1034]:
 
 
 df_test = pd.read_parquet(Path("data") / "final_test.parquet")
 
 
-# In[103]:
+# In[1035]:
 
 
 df_test = _merge_external_data(df_test)
 
 
-# In[104]:
+# In[1036]:
 
 
 test_dates_encoding = _encode_dates(df_test[["date"]])
 df_test = pd.concat([df_test, test_dates_encoding], axis=1) 
 
 
-# In[105]:
+# In[1037]:
 
 
-df_test['counter_age_days'] = df_test['date'] - df_test['counter_installation_date']
-df_test['counter_age_days'] = df_test['counter_age_days'].dt.days
+df_test["counter_name"] = encoder.transform(df_test[["counter_name"]])
 
 
-# In[106]:
+# In[1032]:
 
 
-df_test ['rr3'] = df_test ['rr3'].fillna(df_test ['rr3'].mean())
-df_test ['rr12'] = df_test ['rr12'].fillna(df_test ['rr3'].mean())
-
-
-# In[107]:
-
-
-y_pred = pipe.predict(df_test)
+y_pred = pipe.predict(df_test[selected_features])
 results = pd.DataFrame(
     dict(
         Id=np.arange(y_pred.shape[0]),
@@ -163,4 +185,10 @@ results = pd.DataFrame(
     )
 )
 results.to_csv("submission.csv", index=False)
+
+
+# In[ ]:
+
+
+
 
